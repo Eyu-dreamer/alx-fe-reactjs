4e1469291024e9296d3449c39eeab714de9e503f
eyueal <eyuealayalew35@gmail.com>
2025-02-16 07:43:52 +0300

    c

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import WelcomeMessage from "./components/WelcomeMessage";
+// App.jsx
 import Header from "./Header";
 import MainContent from "./MainContent";
+import Footer from "./Footer";
+
 function App() {
   return (
-    <div>
-      <WelcomeMessage />
-      <Header></Header>
-      <MainContent></MainContent>
-    </div>
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
 
