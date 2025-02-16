27e2d5bce6d5ef86bd93fd09280180f42bafe7cf
eyueal <eyuealayalew35@gmail.com>
2025-02-16 07:40:39 +0300

    one for the second project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header></Header>
+      <MainContent></MainContent>
     </div>
   );
 }
