7b1badcfe4d638dc6de695410a58b225c1bf9805
eyueal <eyuealayalew35@gmail.com>
2025-02-16 07:16:10 +0300

    finally

--- alx-react-app/src/WelcomeMessage.jsx
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -4,7 +4,8 @@ function WelcomeMessage() {
   return (
     <div>
       <h1> Hello eveyone, I am learning React at ALX! </h1>{" "}
-      <p> I am learning about JSX! </p>{" "}
+      <p>This is a simple JSX component</p>
+      <p> I am learning about JSX! </p>
     </div>
   );
 }
