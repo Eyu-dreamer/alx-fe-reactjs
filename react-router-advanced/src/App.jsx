ae9063001099d1f9afeb9dc8829b87c41a762a19
eyueal <eyuealayalew35@gmail.com>
2025-03-14 15:51:05 +0300

    23

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -16,7 +16,29 @@ function App() {
         <h1>Advanced Routing Example</h1>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/post/:postId" element={<BlogPost />} />
+          <Route // src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost'; // Make sure you import BlogPost here
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic route for blog post */}
+        <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+ element={<BlogPost />} />
 
           {/* Protected Route */}
           <Route
