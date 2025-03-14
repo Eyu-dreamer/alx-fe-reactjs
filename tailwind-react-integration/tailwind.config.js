f25013bd46664cea6ecdddaf826fc5b2ae915a55
eyueal <eyuealayalew35@gmail.com>
2025-03-14 04:10:14 +0300

    improved-tailwind-react-integration

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This ensures Tailwind scans all your JavaScript/TypeScript/JSX/TSX files
+    "./public/index.html", // Make sure to scan the HTML file
+  ],
+  darkMode: false, // You can also set this to 'media' or 'class' if you're using dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This ensures Tailwind scans all your JavaScript/TypeScript/JSX/TSX files
+    "./public/index.html", // Make sure to scan the HTML file
+  ],
+  darkMode: false, // You can also set this to 'media' or 'class' if you're using dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
