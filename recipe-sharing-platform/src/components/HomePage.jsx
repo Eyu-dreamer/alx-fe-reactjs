20a8f5add6870175bbb50ffb0ff4a47b93fe0371
eyueal <eyuealayalew35@gmail.com>
2025-03-23 17:42:05 +0300

    lose of calmness

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -26,7 +26,7 @@ function Home() {
   });
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 p-4 m-4 gap-4">
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 m-4 gap-4">
       {recipeCards}
     </div>
   );
